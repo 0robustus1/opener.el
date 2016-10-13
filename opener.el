68677d177f63a2da63db6a9b20c14e0ef291cfe5
Tim Reddehase <robustus@rightsrestricted.com>
2016-10-13 09:02:59 +0200

    try with-eval-after-load.

--- opener.el
+++ opener.el
@@ -138,22 +138,23 @@ nil)."
   (let ((url-or-file (read-string "URL or FILE to open: ")))
     (opener-try-open url-or-file force-buffer #'find-file)))
 
+(with-eval-after-load "evil"
 ;;;###autoload
-(defmacro opener-hook-into-evil ()
-  "Activates definitions of opener in evil.
+  (defun opener-hook-into-evil ()
+    "Activates definitions of opener in evil.
 This means the definition of the :opener ex-state command,
 as well as the redefinition of the gf normal-state mapping to
 map to opener instead."
-  `(require 'evil)
-  `(evil-define-command opener-evil-open (url-or-file &optional bang)
-    "Open URL-OR-FILE. If the url doesn't have the scheme http:// or https:// it
+    (require 'evil)
+    (evil-define-command opener-evil-open (url-or-file &optional bang)
+      "Open URL-OR-FILE. If the url doesn't have the scheme http:// or https:// it
   falls back to be equivalent to :edit"
-    :repeat nil
-    (interactive "<a><!>")
-    (opener-try-open url-or-file bang #'evil-edit))
+      :repeat nil
+      (interactive "<a><!>")
+      (opener-try-open url-or-file bang #'evil-edit))
 
-  `(define-key evil-normal-state-map "gf" 'opener-open-at-point)
-  `(evil-ex-define-cmd "o[pener]" 'opener-evil-open))
+    (define-key evil-normal-state-map "gf" 'opener-open-at-point)
+    (evil-ex-define-cmd "o[pener]" 'opener-evil-open)))
 
 (provide 'opener)
 ;;; opener.el ends here

1a9a737e3f93f8ef22f1e86bc1a9a1bfb482f48d
Tim Reddehase <robustus@rightsrestricted.com>
2016-10-16 14:09:55 +0200

    add estimated version dependency for opener.

--- evil-opener.el
+++ evil-opener.el
@@ -2,7 +2,7 @@
 
 ;; Author: Tim Reddehase <tr@rightsrestricted.com>
 ;; Version: 0.1
-;; Package-Requires: ((evil "20160827.1510"))
+;; Package-Requires: ((evil "20160827.1510") (opener "20161016.948"))
 ;; Keywords: url, http, files
 ;; URL: https://github.com/0robustus1/opener.el
 
